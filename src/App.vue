7b5c81916f620817607933dc0893205679230cb3
Adrianna Guevarra <adrianna.guevarra@sdvi.com>
2022-07-24 22:25:46 -0700

    update header and footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <NavigationBar />
     <router-view />
+    <FooterBar />
   </div>
 </template>
 
 <script>
 import NavigationBar from "@/components/NavigationBar.vue";
+import FooterBar from "@/components/FooterBar.vue";
 export default {
   name: "app",
   components: {
     NavigationBar,
+    FooterBar,
   },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      "desert-purple": "#83789C",
-      white: "#ffffff",
+    extend: {
+      colors: {
+        "desert-purple": "#83789C",
+      },
     },
   },
   plugins: [],
