572ff4f88ee8a75d0c7858e89f5b9d4cbb461887
Adrianna Guevarra <adriannaguevarra@Adriannas-MacBook-Pro-Main.local>
2022-07-24 18:29:43 -0700

    create nav componen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view />
+    <NavigationBar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import NavigationBar from "@/components/NavigationBar.vue";
 export default {
-  name: "HomeView",
+  name: "app",
   components: {
-    HelloWorld,
+    NavigationBar,
   },
 };
 </script>
