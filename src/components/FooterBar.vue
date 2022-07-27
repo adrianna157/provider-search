28f908608ec5b2c4c02804c07e0177758a656991
Adrianna Guevarra <adrianna.guevarra@sdvi.com>
2022-07-27 07:57:29 -0700

    update css

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,16 +1,14 @@
 <template>
-  <footer class="fixed w-full bottom-0 bg-desert-purple pb-6">
+  <footer class="fixed w-full bottom-0 bg-desert-purple pb-6 text-center">
     <div class="container mx-auto px-4">
       <hr class="mb-6 border-b-1 border-gray-700 drop-shadow-lg" />
-      <div
-        class="flex flex-wrap items-center md:justify-between justify-center"
-      >
-        <div class="w-full md:w-4/12 px-4 text-center">
+      <div class="">
+        <div class="px-4">
           <div class="text-sm text-white font-semibold py-1 drop-shadow-lg">
             Copyright © {{ date }}
             <a
               href="https://testlink.org/"
-              class="text-white hover:text-gray-400 text-sm font-semibold py-1"
+              class="text-white hover:text-gray-400 text font-semibold py-1"
               >COIPA</a
             >
           </div>
